9f030704d4866546890050f44eb92aaa1a8e4536
Jorge Oney <guinodo@gmail.com>
2021-12-07 14:03:51 -0300

    adicionaodo v1 da api

--- infra/src/main/java/br/com/guinodo/votos/http/server/PautaController.java
+++ infra/src/main/java/br/com/guinodo/votos/http/server/PautaController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/pauta")
+@RequestMapping("/v1/pauta")
 public class PautaController {
 
     @PostMapping
